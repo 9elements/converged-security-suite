c6bb70e8ec05c0c06c087a3e3fb3c69a4d184d43
Kai Michaelis <kai.michaelis@9elements.com>
2019-06-25 11:38:02 +0200

    add additional test data for LCP parser

--- pkg/api/lcp_test.go
+++ pkg/api/lcp_test.go
@@ -7,29 +7,43 @@ import (
 	"github.com/9elements/txt-suite/pkg/api"
 )
 
-func TestLCPPolicyParser(t *testing.T) {
+func TestLCPParser(t *testing.T) {
 	file, err := ioutil.ReadFile("./tests/pol.bin")
 	if err != nil {
-		t.Errorf("LCPPolicyParser() read failed: %v", err)
+		t.Fatalf("LCPParser() read failed: %v", err)
 	}
 
 	pol, err := api.ParsePolicy(file)
 	if err != nil {
-		t.Errorf("LCPPolicyParser() failed: %v", err)
+		t.Fatalf("LCPParser() failed: %v", err)
 	}
 
 	pol.PrettyPrint()
 }
 
-func TestLCPPolicyDataParser(t *testing.T) {
+func TestLCPDataParser(t *testing.T) {
 	file, err := ioutil.ReadFile("./tests/poldata.bin")
 	if err != nil {
-		t.Errorf("LCPPolicyDataParser() failed: %v", err)
+		t.Fatalf("LCPDataParser() failed: %v", err)
 	}
 
 	poldata, err := api.ParsePolicyData(file)
 	if err != nil {
-		t.Errorf("LCPPolicyDataParser() failed: %v", err)
+		t.Fatalf("LCPDataParser() failed: %v", err)
+	}
+
+	poldata.PrettyPrint()
+}
+
+func TestLCPDataParser2(t *testing.T) {
+	file, err := ioutil.ReadFile("./tests/poldata2.bin")
+	if err != nil {
+		t.Fatalf("LCPDataParser() failed: %v", err)
+	}
+
+	poldata, err := api.ParsePolicyData(file)
+	if err != nil {
+		t.Fatalf("LCPDataParser() failed: %v", err)
 	}
 
 	poldata.PrettyPrint()
