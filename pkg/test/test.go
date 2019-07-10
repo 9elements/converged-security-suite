e2e02f22e0481fef7fb45c583c38f6dde9238f76
Kai Michaelis <kai.michaelis@9elements.com>
2019-07-10 11:52:04 +0200

    flush test runner output

--- pkg/test/test.go
+++ pkg/test/test.go
@@ -1,7 +1,9 @@
 package test
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 type Test struct {
@@ -11,17 +13,24 @@ type Test struct {
 }
 
 func (self *Test) Run() bool {
+	f := bufio.NewWriter(os.Stdout)
+
 	fmt.Printf("%s: ", self.name)
+	f.Flush()
+
 	rc, err := self.function()
 	if err != nil {
 		fmt.Printf("ERROR\n\t%s\n", err)
+		f.Flush()
 		return false
 	}
+
 	if rc {
 		fmt.Println("OK")
 	} else {
 		fmt.Println("FAIL\n\t%s", "TODO")
 	}
+	f.Flush()
 
 	return rc
 }
