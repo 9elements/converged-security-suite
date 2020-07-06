be43b8abcb57ebd165232094f97d812f7279877c
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-07-06 17:23:23 +0200

    pkg/api: Add unit tests for ReadACMStatus
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- pkg/api/txt_test.go
+++ pkg/api/txt_test.go
@@ -5,9 +5,7 @@ import (
 )
 
 func TestTXT(t *testing.T) {
-	t.Skip()
-
-	txtAPI := GetApi()
+	txtAPI := GetPcMock(func(addr int64) byte { return MockPCReadMemory(addr) })
 
 	got, err := FetchTXTRegs(txtAPI)
 
@@ -16,4 +14,113 @@ func TestTXT(t *testing.T) {
 	}
 
 	t.Logf("TXT: %+v", got)
+}
+
+func TestTxtApi_ParseTXTRegs(t *testing.T) {
+	txtAPI := GetPcMock(func(addr int64) byte { return MockPCReadMemory(addr) })
+
+	data, err := FetchTXTRegs(txtAPI)
+
+	got, err := ParseTXTRegs(data)
+
+	if err != nil {
+		t.Errorf("ParseTXTRegs() failed: %v", err)
+	}
+
+	t.Logf("TXT: %+v", got)
+}
+
+func TestReadACMStatus(t *testing.T) {
+
+	type fields struct {
+		ACMStatus [4]byte
+	}
+
+	tests := []struct {
+		name          string
+		fields        fields
+		wantErr       bool
+		wantACMStatus ACMStatus
+	}{
+		{
+			"Invalid register",
+			fields{[4]byte{0, 0, 0, 0}},
+			false,
+			ACMStatus{
+				false,
+				0,
+				false,
+				0,
+				0,
+				0,
+			},
+		},
+		{
+			"Valid register",
+			fields{[4]byte{0, 0, 0, 0x80}},
+			false,
+			ACMStatus{
+				true,
+				0,
+				false,
+				0,
+				0,
+				0,
+			},
+		},
+		{
+			"Minor error",
+			fields{[4]byte{0, 0, 0xaa, 0x8b}},
+			false,
+			ACMStatus{
+				true,
+				0xbaa,
+				false,
+				0,
+				0,
+				0,
+			},
+		},
+		{
+			"Major error",
+			fields{[4]byte{0, 0xc, 0x00, 0x80}},
+			false,
+			ACMStatus{
+				true,
+				0,
+				false,
+				0x3,
+				0,
+				0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			txtAPI := GetPcMock(func(addr int64) byte {
+				if addr >= (txtPublicSpace+txtACMStatus) && addr < int64(txtPublicSpace+txtACMStatus+len(tt.fields.ACMStatus)) {
+					addr -= (txtPublicSpace + txtACMStatus)
+					t.Logf("%x\n", tt.fields.ACMStatus[addr])
+					return tt.fields.ACMStatus[addr]
+				}
+				return MockPCReadMemory(addr)
+			})
+
+			data, err := FetchTXTRegs(txtAPI)
+			if err != nil {
+				t.Errorf("FetchTXTRegs() failed: %v", err)
+				t.Skip()
+			}
+			got, err := ReadACMStatus(data)
+			if err != nil && !tt.wantErr {
+				t.Errorf("ReadACMStatus() failed, got unexpected error: %v", err)
+			}
+			if err == nil && tt.wantErr {
+				t.Errorf("ReadACMStatus() failed, expected an error")
+			}
+			if got != tt.wantACMStatus {
+				t.Errorf("Result missmatch: got %v, want %v", got, tt.wantACMStatus)
+			}
+		})
+	}
 }
