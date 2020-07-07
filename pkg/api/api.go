35a0d3d4563071c836b7236aab720f227a525416
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-07-07 07:43:39 +0200

    Add comments to API interface

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,9 +3,11 @@ package api
 import "io"
 
 type ApiInterfaces interface {
+	// cpu_whitelist.go - cpu_blacklist.go
 	CPUBlacklistTXTSupport() bool
 	CPUWhitelistTXTSupport() bool
 
+	// cpuid.go
 	VersionString() string
 	HasSMX() bool
 	HasVMX() bool
@@ -14,11 +16,14 @@ type ApiInterfaces interface {
 	CPUSignature() uint32
 	CPULogCount() uint32
 
+	// e820.go
 	IsReservedInE810(start uint64, end uint64) (bool, error)
 
+	// iommu.go
 	LookupIOAddress(addr uint64, regs VTdRegisters) ([]uint64, error)
 	AddressRangesIsDMAProtected(first, end uint64) (bool, error)
 
+	// msr.go
 	HasSMRR() (bool, error)
 	GetSMRRInfo() (SMRR, error)
 	IA32FeatureControlIsLocked() (bool, error)
@@ -27,6 +32,7 @@ type ApiInterfaces interface {
 	TXTLeavesAreEnabled() (bool, error)
 	IA32DebugInterfaceEnabledOrLocked() (bool, bool, bool, error)
 
+	// pci.go
 	PCIReadConfigSpace(bus int, device int, devFn int, off int, buf interface{}) error
 	PCIReadConfig16(bus int, device int, devFn int, off int) (uint16, error)
 	PCIReadConfig32(bus int, device int, devFn int, off int) (uint32, error)
@@ -35,10 +41,12 @@ type ApiInterfaces interface {
 	ReadHostBridgeTseg() (uint32, uint32, error)
 	ReadHostBridgeDPR() (DMAProtectedRange, error)
 
+	// phys.go
 	ReadPhys(addr int64, data UintN) error
 	ReadPhysBuf(addr int64, buf []byte) error
 	WritePhys(addr int64, data UintN) error
 
+	// tpm.go
 	NVReadAll(conn io.ReadWriteCloser, index uint32) []byte
 }
 
