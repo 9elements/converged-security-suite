0c5bf0865bb4e5af422847a8385614cb351be45e
Christopher Meis <christopher.meis@9elements.com>
2021-03-24 17:19:17 +0100

    Remove ValidateKM function. Function wasn't used nor correctly implemented
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/provisioning/bg/unmarshal.go
+++ pkg/provisioning/bg/unmarshal.go
@@ -2,7 +2,6 @@ package bg
 
 import (
 	"errors"
-	"fmt"
 	"io"
 
 	"github.com/9elements/converged-security-suite/v2/pkg/intel/metadata/manifest/bootpolicy"
@@ -38,18 +37,4 @@ func ParseKM(reader io.Reader) (*key.Manifest, error) {
 		return nil, err
 	}
 	return km, nil
-}
-
-// ValidateKM reads from a binary source, parses into the key manifest structure
-// and validates the structure
-func ValidateKM(reader io.Reader) error {
-	km := &key.Manifest{}
-	_, err := km.ReadFrom(reader)
-	if err != nil && !errors.Is(err, io.EOF) {
-		return err
-	}
-	if km.PubKeyHashAlg != km.KeyAndSignature.Signature.HashAlg {
-		return fmt.Errorf("header pubkey hash algorithm doesn't match signature hash")
-	}
-	return km.Validate()
 }
