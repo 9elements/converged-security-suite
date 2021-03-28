346ab52a18d76ff62e7a019871627af676f3168d
Ilya <ilyaarzamartsev@fb.com>
2021-03-28 22:30:59 +0100

    Tpm12 support (#195)
    Add new measurements flow FlowIntelLegacyTXTEnabledTPM12
    Add TPM type detection based on firmware&registers

--- pkg/pcr/get_measurements_pcr0_legacytxtenabled.go
+++ pkg/pcr/get_measurements_pcr0_legacytxtenabled.go
@@ -1,10 +1,14 @@
 package pcr
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/binary"
+	"fmt"
 
 	"github.com/9elements/converged-security-suite/v2/pkg/errors"
 	"github.com/9elements/converged-security-suite/v2/pkg/intel/metadata/fit"
+	"github.com/google/go-tpm/tpm2"
 )
 
 func MeasureInit() *Measurement {
@@ -26,7 +30,6 @@ func MeasureACMDate(fitEntries []fit.Entry) (*Measurement, error) {
 			if err != nil {
 				mErr.Add(err)
 			}
-
 			if data == nil {
 				continue
 			}
@@ -38,6 +41,46 @@ func MeasureACMDate(fitEntries []fit.Entry) (*Measurement, error) {
 		}
 	}
 
+	return &m, mErr.ReturnValue()
+}
+
+func MeasureACMDateInPlace(hashAlg tpm2.Algorithm, fitEntries []fit.Entry) (*Measurement, error) {
+	m := Measurement{
+		ID: MeasurementIDACMDateInPlace,
+	}
+
+	var hashSize int
+	switch hashAlg {
+	case tpm2.AlgSHA1:
+		hashSize = sha1.New().Size()
+	case tpm2.AlgSHA256:
+		hashSize = sha256.New().Size()
+	default:
+		return nil, fmt.Errorf("unknown hash algorithm: %v", hashAlg)
+	}
+
+	mErr := &errors.MultiError{}
+	for _, fitEntry := range fitEntries {
+		switch fitEntry := fitEntry.(type) {
+		case *fit.EntrySACM: // startup AC module entry
+
+			mErr.Add(fitEntry.HeadersErrors...)
+			data, err := fitEntry.ParseData()
+			if err != nil {
+				mErr.Add(err)
+			}
+			if data == nil {
+				continue
+			}
+
+			offset := fitEntry.GetDataOffset() + uint64(data.DateBinaryOffset())
+			length := uint64(binary.Size(data.GetDate()))
+
+			m.Data = append(m.Data, *NewRangeDataChunk(DataChunkIDUndefined, offset, length))
+			padding := make([]byte, uint64(hashSize)-length)
+			m.Data = append(m.Data, *NewStaticDataChunk(DataChunkIDUndefined, padding))
+		}
+	}
 	return &m, mErr.ReturnValue()
 }
 
