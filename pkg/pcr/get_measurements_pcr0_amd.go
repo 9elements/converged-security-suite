92b4758a59735885c13feaf27550791ffa94c0fe
Ilya Arzamartsev <ilyaarzamartsev@fb.com>
2021-07-17 19:21:53 +0100

    BIOS directory table level 1&2 measurements

--- pkg/pcr/get_measurements_pcr0_amd.go
+++ pkg/pcr/get_measurements_pcr0_amd.go
@@ -1,1 +1,54 @@
 package pcr
+
+import (
+	"encoding/binary"
+	"fmt"
+	amd_manifest "github.com/9elements/converged-security-suite/v2/pkg/amd/manifest"
+	pkgbytes "github.com/9elements/converged-security-suite/v2/pkg/bytes"
+)
+
+// MeasureBIOSDirectoryHeader constructs measurements of BIOS Directory table header
+func MeasureBIOSDirectoryHeader(table *amd_manifest.BIOSDirectoryTable, biosDirectoryTableRange pkgbytes.Range) (*Measurement, error) {
+	if table == nil {
+		return nil, nil
+	}
+	var id MeasurementID
+	switch table.BIOSCookie {
+	case amd_manifest.BIOSDirectoryTableCookie:
+		id = MeasurementIDBIOSDirectoryLevel1Header
+	case amd_manifest.BIOSDirectoryTableLevel2Cookie:
+		id = MeasurementIDBIOSDirectoryLevel2Header
+	default:
+		return nil, fmt.Errorf("unknown bios table cookie: '%X'", table.BIOSCookie)
+	}
+	headerSize := uint64(binary.Size(table.BIOSDirectoryTableHeader))
+	if headerSize > biosDirectoryTableRange.Length {
+		return nil, fmt.Errorf("bios table is too short: '%d'", biosDirectoryTableRange.Length)
+	}
+	return NewRangeMeasurement(id, biosDirectoryTableRange.Offset, headerSize), nil
+}
+
+// MeasureBIOSDirectoryTable constructs measurements of BIOS Directory table
+func MeasureBIOSDirectoryTable(table *amd_manifest.BIOSDirectoryTable, biosDirectoryTableRange pkgbytes.Range) (*Measurement, error) {
+	if table == nil {
+		return nil, nil
+	}
+
+	var id MeasurementID
+	switch table.BIOSCookie {
+	case amd_manifest.BIOSDirectoryTableCookie:
+		id = MeasurementIDBIOSDirectoryLevel1
+	case amd_manifest.BIOSDirectoryTableLevel2Cookie:
+		id = MeasurementIDBIOSDirectoryLevel2
+	default:
+		return nil, fmt.Errorf("unknown bios table cookie: '%X'", table.BIOSCookie)
+	}
+	headerSize := uint64(binary.Size(table.BIOSDirectoryTableHeader))
+	if headerSize > biosDirectoryTableRange.Length {
+		return nil, fmt.Errorf("bios table is too short: '%d'", biosDirectoryTableRange.Length)
+	}
+	return NewRangeMeasurement(
+		id,
+		biosDirectoryTableRange.Offset+headerSize,
+		biosDirectoryTableRange.Length-headerSize), nil
+}
