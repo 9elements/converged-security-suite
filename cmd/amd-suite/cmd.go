e9a548185c9b23478337a5788c7fdda45bbb9149
Marco Guerri <marcoguerri@fb.com>
2021-09-08 08:22:59 -0700

    [attestation][amd] Command to validate entries of PSP elements
    We need a generic command that takes in input a list of PSP entries
    IDs which are supposed to be signed and validates their signature. This should
    be the main command used for firmawre validation before flashing.

--- cmd/amd-suite/cmd.go
+++ cmd/amd-suite/cmd.go
@@ -17,9 +17,15 @@ type showKeyDBCmd struct {
 	FwPath string `arg required name:"fwpath" help:"Path to UEFI firmware image." type:"path"`
 }
 
+type validatePSPEntriesCmd struct {
+	FwPath     string   `arg required name:"fwpath" help:"Path to UEFI firmware image." type:"path"`
+	PSPEntries []string `arg required name:"validate-psp-entries" help:"Validates the signature of PSP entries given as argument." type:"list"`
+}
+
 var cli struct {
-	Debug     bool         `help:"Enable debug mode"`
-	ShowKeyDB showKeyDBCmd `cmd help:"Shows content of Key Database"`
+	Debug              bool                  `help:"Enable debug mode"`
+	ShowKeyDB          showKeyDBCmd          `cmd help:"Shows content of Key Database"`
+	ValidatePSPEntries validatePSPEntriesCmd `cmd help:"Validates signatures of PSP entries"`
 }
 
 func (s *showKeyDBCmd) Run(ctx *context) error {
@@ -35,4 +41,22 @@ func (s *showKeyDBCmd) Run(ctx *context) error {
 
 	fmt.Println(keyDB.String())
 	return nil
+}
+
+func (v *validatePSPEntriesCmd) Run(ctx *context) error {
+	firmware, err := uefi.ParseUEFIFirmwareFile(v.FwPath)
+	if err != nil {
+		return fmt.Errorf("could not parse firmware image: %w", err)
+	}
+
+	signatureValidations, err := psb.ValidatePSPEntries(firmware, v.PSPEntries)
+	if err != nil {
+		return err
+	}
+
+	for _, validation := range signatureValidations {
+		fmt.Println(validation.String())
+	}
+	return nil
+
 }
