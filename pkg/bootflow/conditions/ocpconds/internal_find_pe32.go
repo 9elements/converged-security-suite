1d13f4ab0960c717e4077380ce27a4bc95bfa80d
Dmitrii Okunev <xaionaro@meta.com>
2023-03-29 19:53:02 +0100

    [bootflow] Bugfix for finding PE32 files

--- pkg/bootflow/conditions/ocpconds/internal_find_pe32.go
+++ pkg/bootflow/conditions/ocpconds/internal_find_pe32.go
@@ -10,14 +10,13 @@ type visitorFindPE32orTE struct {
 
 var _ uefi.Visitor = (*visitorFindPE32orTE)(nil)
 
-func (visitorFindPE32orTE) Run(uefi.Firmware) error {
-	return nil
+func (v *visitorFindPE32orTE) Run(fw uefi.Firmware) error {
+	return v.Visit(fw)
 }
 func (v *visitorFindPE32orTE) Visit(fw uefi.Firmware) error {
 	if section, ok := fw.(*uefi.Section); ok {
 		switch section.Header.Type {
-		case uefi.SectionTypePE32:
-		case uefi.SectionTypeTE:
+		case uefi.SectionTypePE32, uefi.SectionTypeTE:
 			v.Found = section
 			return nil
 		}
