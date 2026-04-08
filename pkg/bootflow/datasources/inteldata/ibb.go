05cc8369c1d8d20932e26d83cf19973386a08afb
Michal Gorlas <michal.gorlas@9elements.com>
2026-04-08 15:22:09 +0200

    feat: pkg/bootflow/datasources/inteldata/ibb.go: adjust to unified CBnT
    Signed-off-by: Michal Gorlas <michal.gorlas@9elements.com>

--- pkg/bootflow/datasources/inteldata/ibb.go
+++ pkg/bootflow/datasources/inteldata/ibb.go
@@ -7,6 +7,8 @@ import (
 	"github.com/9elements/converged-security-suite/v2/pkg/bootflow/systemartifacts/biosimage"
 	"github.com/9elements/converged-security-suite/v2/pkg/bootflow/systemartifacts/biosimage/accessor/intelbiosimage"
 	"github.com/9elements/converged-security-suite/v2/pkg/bootflow/types"
+	pkgbytes "github.com/linuxboot/fiano/pkg/bytes"
+	bootpolicy "github.com/linuxboot/fiano/pkg/intel/metadata/cbnt/bootpolicy"
 )
 
 // IBB implements types.DataSource by referencing to
@@ -27,7 +29,17 @@ func (IBB) Data(ctx context.Context, s *types.State) (*types.Data, error) {
 		return nil, fmt.Errorf("unable to get BPM: %w", err)
 	}
 
-	ranges := bpm.IBBDataRanges(intelFW.SystemArtifact().Size())
+	var ranges pkgbytes.Ranges
+	bpmP := *bpm
+
+	if bpmBg, ok := bpmP.(*bootpolicy.ManifestBG); ok {
+		ranges = bpmBg.IBBDataRanges(intelFW.SystemArtifact().Size())
+	}
+
+	if bpmCBnt, ok := bpmP.(*bootpolicy.ManifestCBnT); ok {
+		ranges = bpmCBnt.IBBDataRanges(intelFW.SystemArtifact().Size())
+	}
+
 	addrMapper := biosimage.PhysMemMapper{}
 	ranges = addrMapper.UnresolveFullImageOffset(intelFW.SystemArtifact(), ranges...)
 
